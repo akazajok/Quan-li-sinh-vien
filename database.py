ef042165dec46e5be2eeca62f0fe668f19187e11
Khanh Vu <vungockhanh8anh@gmail.com>
2025-12-06 16:53:50 +0700

    Code btn thêm sinh viên

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 import csv
 import os
 
+from numpy.f2py.crackfortran import expectbegin
+from function_dialog import Ui_Dialog
+
 class Student:
     def __init__(self, ID, full_name, DateOfBirth, Gender, Class, GPA):
         self.ID = ID
@@ -8,27 +11,60 @@ class Student:
         self.DateOfBirth = DateOfBirth
         self.Gender = Gender
         self.Class = Class
-        self.GPA = GPA
+        self.GPA = float(GPA)
 
 class CsvData:
     def __init__(self, filename = "database.csv"):
         self.csv_file = filename
+        self.function_dialog = Ui_Dialog()
 
     def get_data_Csv(self):
         list_student = []
-        with open(self.csv_file, 'r', encoding="utf-8-sig", newline='') as csvfile:
-            for line in csvfile:
-                try:
-                    ID, full_name, DateOfBirth, Gender, Class, GPA = line.strip().split(',')
-                    GPA = float(GPA)
-                    list_student.append(Student(ID, full_name, DateOfBirth, Gender, Class, GPA))
-                except:
-                    pass
+        try :
+            with open(self.csv_file, 'r', encoding="utf-8-sig", newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                for line in reader:
+                    if len(line) >=6 :
+                        try:
+                            ID = line[0]
+                            full_name = line[1]
+                            DateOfBirth = line[2]
+                            Gender = line[3]
+                            Class = line[4]
+                            GPA = line[5]
+                            list_student.append(Student(ID, full_name, DateOfBirth, Gender, Class, GPA))
+                        except:
+                            pass
+        except Exception as e:
+            print(f"Lỗi khi đọc file: {e}")
+
         return list_student
 
     def update_data_Csv(self, list_student):
-        with open(self.csv_file, 'w', encoding="utf-8-sig", newline='') as csvfile:
-            for student in list_student:
-                csvfile.write(
-                    f"{student.ID},{student.full_name},{student.DateOfBirth},{student.Gender},{student.Class},{student.GPA}\n")
+        try:
+            with open(self.csv_file, 'w', encoding="utf-8-sig", newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                for student in list_student:
+                    writer.writerow([
+                        student.ID,  student.full_name,  student.DateOfBirth,
+                        student.Gender, student.Class, student.GPA
+                    ])
+        except Exception as e:
+            print(f"Lỗi khi đọc file: {e}")
 
+    def add_student_to_csv(self):
+        ID = self.function_dialog.lineEdit_ID.text().strip()
+        full_name = self.function_dialog.lineEdit_fullName.text().strip()
+        DateOfBirth = self.function_dialog.dateEdit_DateBirth.text().strip()
+        Gender = self.function_dialog.comboBox_gender.text().strip()
+        Class = self.function_dialog.lineEdit_class.text().strip()
+        GPA = self.function_dialog.lineEdit_GPA.text().strip()
+        student = Student(ID, full_name, DateOfBirth, Gender, Class, GPA)
+        try:
+            with open(self.csv_file, 'a', encoding="utf-8-sig", newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow([ ID, full_name, DateOfBirth, Gender, Class, GPA])
+        except Exception as e:
+            print(f"Lỗi khi đọc file: {e}")
+        return student
+            
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog, QMessageBox
+from PyQt5.QtCore import QDate
 
 from Student_management import Ui_MainWindow  # File giao diện gốc (auto-generated)
+from function_dialog import Ui_Dialog # file # File giao diện cập nhật sinh viên
 from config_ui import TableHelper  # File trợ lý bạn vừa tạo
 from database import CsvData
 
@@ -10,11 +12,14 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.function_dialog = Ui_Dialog()
 
         self.database = CsvData()
         self.list_student = self.database.get_data_Csv()
         # Load dữ liệu ngay khi mở
         self.load_data_to_table()
+        # Thêm dữ liệu
+        self.ui.btn_add_student.clicked.connect(self.add_student_to_table)
 
     def load_data_to_table(self):
         table = self.ui.studentsTableWidget
@@ -30,10 +35,36 @@ class MainWindow(QMainWindow):
             table.setItem(row_index, 2, QTableWidgetItem(student.DateOfBirth))
             table.setItem(row_index, 3, QTableWidgetItem(student.Gender))
             table.setItem(row_index, 4, QTableWidgetItem(student.Class))
-            table.setItem(row_index, 5, QTableWidgetItem(str(student.GPA)))
+            table.setItem(row_index, 5, QTableWidgetItem(f"{student.GPA:.2f}"))
             #Thêm nút xóa, sửa
             TableHelper.add_button_to_tableWidget(self, self.ui.studentsTableWidget, row_index, 6)
         table.setSortingEnabled(True) # Bật lại sắp xếp để user bấm vào tiêu đề cột
+
+    def add_student_to_table(self):
+        #load giao diện thêm, sửa sinh viên ( function_dialog )
+        self.function_dialog.setupUi(QDialog())
+        def check_data() :
+            student = self.database.add_student_to_csv()
+            if ( not student.ID and not student.full_name and not student.DateOfBirth
+            and not student.Gender and not student.Class):
+                QMessageBox.warning( QDialog(), "Cảnh báo" , "Vui lòng nhập ĐẦY ĐỦ thông tin sinh viên")
+                return
+
+            for inf in self.list_student:
+                if student.ID == inf.ID :
+                    QMessageBox.critical( QDialog(), "Lỗi" , f"Mã sinh viên {inf.ID} đã tồn tại!")
+                    return
+
+            self.list_student.append(student)
+            self.load_data_to_table()
+            QMessageBox.information(self, "Thông báo" , "Thêm sinh viên thành công")
+            QDialog().accept() #Đóng dialog
+        # Bấm nút hủy thoát khỏi giao diện
+        self.function_dialog.btn_cancel.clicked.connect( QDialog().reject)
+        # Cập nhật sinh viên
+        self.function_dialog.btn_update_infor.clicked.connect( check_data )
+        #-----Hiên thị Dialog---------
+        QDialog().exec_()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
